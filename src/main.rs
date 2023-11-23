45f06d99e7b1b71fa6f22bbb212f6c6b33c9d3fa
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-11-23 16:20:18 +0100

    add: fix compiler warnings

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
-// Import Mio
-extern crate mio;
-use mio::{Poll, Events};
-use std::process::exit;
-
-// Represents a websocket server 
-struct WebSocketServer;
-
-
 fn main() {
-    // Hold all events for a poll
-    let mut events = Events::with_capacity(1024);
 
-    // Allows a program to monitor a large number of requests 
-    let mut poll =  match Poll::new(){
-        Ok(pull) => pull,
-        Err(e) => {
-            eprint!("ERROR: creating poll: {e}");
-            exit(1);
-        },
-    };
+    println!("Hello World!");
+    
 }
