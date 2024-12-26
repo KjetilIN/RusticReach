b8e16b899b72d70dc298f9bf2d5d96302a9b0613
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-12-26 23:19:18 +0100

    add: basic setup with actix web application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main(){
-    println!("Hello world");
+use actix_web::{dev::Service, get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap_fn(|req, srv | {
+                // Logging method 
+                println!("{} request: {}",req.method(), req.path());
+
+                // Calling the route for the given request 
+                srv.call(req)
+            })
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
