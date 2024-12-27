3d0ad9de8a90c854d4a5a2136ba8ab1d5ca8e57c
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-12-27 23:19:10 +0100

    add: state of the client, with connect/disconnect command

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,41 @@
 use std::{io::{self, Write}, process::{exit, Command}};
 
 const COMMAND_LINE_SYMBOL: &str = "$";
+const MESSAGE_LINE_SYMBOL: &str = ">";
+const MESSAGE_COMMAND_SYMBOL: &str = "/";
 const INFO_LOG: &str = "[INFO]";
 const ERROR_LOG: &str = "[ERROR]";
 
 
-fn handle_command(command:&str){
+struct WebSocketConnection;
+
+
+#[derive(Debug, PartialEq, Eq)]
+enum WebSocketState{
+    Connected,
+    Ready, 
+}
+
+struct WebSocketClient{
+    state: WebSocketState
+}
+
+impl WebSocketClient {
+    pub fn new() -> Self{
+        Self { state: WebSocketState::Ready }
+    }
+
+    pub fn get_state(&self) -> &WebSocketState{
+        return &self.state; 
+    }
+
+    pub fn set_state(&mut self, new_state: WebSocketState){
+        self.state = new_state; 
+    }
+}
+
+
+fn handle_client_command(command:&str, ws_client: &mut WebSocketClient){
     let command_parts: Vec<&str> = command.split_ascii_whitespace().collect(); 
 
     match command_parts.as_slice() {
@@ -23,6 +53,14 @@ fn handle_command(command:&str){
             let ip = command_parts[1];
             println!("{} Connecting to server at IP {}", INFO_LOG, ip);
 
+
+            // Do connection
+
+            // Connected!
+
+            // Set state of the client to connected
+            ws_client.set_state(WebSocketState::Connected);
+
         }
         _ => {
             println!("{} Unknown command", ERROR_LOG);
@@ -31,12 +69,43 @@ fn handle_command(command:&str){
 
 }
 
+
+fn handle_message_commands(input:&str, ws_client: &mut WebSocketClient){
+    // Message command only if the command starts with the command symbol
+    // This allows users to execute commands when they are messaging
+    if input.starts_with(MESSAGE_COMMAND_SYMBOL){
+        // Handle given command
+        match input {
+            "/disconnect" => {
+                println!("{} Disconnecting...", INFO_LOG);
+                ws_client.set_state(WebSocketState::Ready);
+            },
+            _ => {
+                println!("{} Unknown command", ERROR_LOG);
+            }
+        }
+
+    }else{
+        // The input is text and should be sent to the server
+        // TODO: send message to server
+    }
+}
+
+
+
 #[tokio::main]
 async fn main() {
+    // Create a new web socket client instance
+    let mut ws_client = WebSocketClient::new(); 
+
     // Infinite input loop
     loop {
         // Print the command line symbol
-        print!("{} ", COMMAND_LINE_SYMBOL);
+        if *ws_client.get_state() == WebSocketState::Connected{
+            print!("{} ", MESSAGE_LINE_SYMBOL);
+        }else{
+            print!("{} ", COMMAND_LINE_SYMBOL);
+        }
 
         // Flush the standard output to make sure the symbol appears immediately
         std::io::stdout().flush().expect("Failed to flush stdout");
@@ -51,12 +120,12 @@ async fn main() {
         // Trim input
         let trimmed_input = input.trim();
 
-        // Handle the command
-        handle_command(trimmed_input);
-
-        // Exit condition (optional)
-        if trimmed_input == "exit" {
-            break;
+        // If the current state is connected, we handle input as messages to the server
+        // Otherwise we handle inputs as commands
+        if *ws_client.get_state() == WebSocketState::Connected{
+            handle_message_commands(trimmed_input, &mut ws_client);
+        }else{
+            handle_client_command(trimmed_input, &mut ws_client);
         }
     }
 }
