c677cd087bebc8e421ef1fe604d9ea21ca82d6d3
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-12-30 17:50:52 +0100

    fix: client config to use correct yaml convention

--- src/client/config.rs
+++ src/client/config.rs
@@ -1,34 +1,21 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use serde::{Deserialize, Serialize};
 use serde_yaml::Value;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct ClientConfig {
-    #[serde(rename = "user-name")]
     user_name: String,
-
-    #[serde(rename = "hash_pass")]
     hash_pass: String,
-
-    #[serde(rename = "user-token")]
     user_token: String,
-
-    #[serde(rename = "validate-server-repo")]
     validate_server_repo: bool,
-
-    #[serde(rename = "default-server")]
     default_server: DefaultServer,
-
-    #[serde(rename = "room_aliases", default)]
-    room_aliases: std::collections::HashMap<String, String>,
+    room_aliases: HashMap<String, String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct DefaultServer {
-    #[serde(rename = "server-ip")]
     server_ip: String,
-    #[serde(rename = "auto-connect")]
     auto_connect: bool,
 }
 
